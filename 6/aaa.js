de3833b36a26d57312888a3e022fcd212e44b4e1
Lu <lumingxian888@gmail.com>
2026-04-23 21:55:05 +0800

    Update aaa.js

--- 6/aaa.js
+++ 6/aaa.js
@@ -5875,7 +5875,7 @@ function Top(q1,q2,q3,q4,q5) {
                           results.push(`無(${target})`);
                     }
         });
-        console.log(results.join(", "));
+        document.write(results.join(", "));
 }
 function TT(x){
        var xz= Array(40).fill(0);
