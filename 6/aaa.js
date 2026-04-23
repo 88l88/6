1e653e39f12b1bf16d2a6311c400073ecb89c9d6
Lu <lumingxian888@gmail.com>
2026-04-23 16:31:56 +0800

    Update aaa.js

--- 6/aaa.js
+++ 6/aaa.js
@@ -5844,3 +5844,34 @@ var aaa=[
 ["2007-01-02(二)",["02","12","33","36","37"],"096002",["33","02","37","12","36"]],
 ["2007-01-01(一)",["09","11","27","28","38"],"096001",["27","38","09","11","28"]]
 ];
+function myTop(q1,q2,q3,q4,q5) {
+        if (typeof aaa === 'undefined') return;
+        qs = [q1, q2, q3, q4, q5];
+        let results = [];
+        qs.forEach(q => {
+                   let target = q.toString().padStart(2, '0');
+                   let stats = {};
+                   for(let n=1; n<=39; n++) stats[n.toString().padStart(2, '0')] = 0;
+                   for(let i = 1; i < aaa.length; i++){
+                         if(aaa[i][1].includes(target)){
+                             aaa[i-1][1].forEach(num => { if(stats[num] !== undefined) stats[num]++; });
+                          }
+                    }
+                   let maxCount = -1;
+                   let topNum = "";
+                   for(let n in stats){
+                         if(stats[n] > maxCount){
+                               maxCount = stats[n];
+                               topNum = n;
+                         }else if(stats[n] === maxCount && maxCount > 0){
+                               topNum += "." + n;
+                         }
+                    }
+                    if(maxCount > 0){
+                          results.push(`${topNum}(${target})`);
+                    }else{
+                          results.push(`無(${target})`);
+                    }
+        });
+       document.write(results.join(", "));
+}
