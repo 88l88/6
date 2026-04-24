ba5042c6590f11dbb8ebfea0474cf0af00041db6
Lu <lumingxian888@gmail.com>
2026-04-24 09:09:07 +0800

    Update aaa.js

--- 6/aaa.js
+++ 6/aaa.js
@@ -5915,4 +5915,8 @@ function Top(nums) {
     // 5. 直接印出結果
     document.write(results.join(", "));
 }
-
+function TT(x){
+       var xz= Array(40).fill(0);
+       aaa.slice(0, x).map(r =>r[1].map(n => xz[Number(n)]++));
+       return xz.slice(1,40);
+}
